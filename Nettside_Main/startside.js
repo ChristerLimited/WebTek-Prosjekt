daec3c595983e4b79aba1d7c575364589a448283
Peder Grundvold <pederbg@stud.ntnu.no>
2016-11-03 15:34:57 +0100

    add javascript openCheck

--- Nettside_Main/startside.js
+++ Nettside_Main/startside.js
@@ -1,10 +1,45 @@
 /**
  * Created by PederGB on 03.11.2016.
  */
-const image1 = document.getElementById("image2")
+function checkOpen() {
+    var today = new Date();
+    var hour = today.getHours();
+    var min = today.getMinutes();
+    var day = today.getDay();
+    var timeinMin = (hour*60)+min
+    console.log(timeinMin)
+    console.log(day)
 
-function link(image1){
-    
+    /* -------- For weekday --------- */
+    if (day < 6) {
+        if ((1140 - timeinMin < 60) && (timeinMin < 1140)) {
+            yolo.innerHTML = "Stenger snart"
+        }
+        else if ((timeinMin > 540) && (timeinMin < 1140)) {
+            yolo.innerHTML = "Åpent nå"
+        }
+        else {
+            yolo.innerHTML = "Stengt"
+        }
+    }
+
+
+    /* -------- For weekday --------- */
+    else if (day == 6) {
+        if ((960 - timeinMin < 60) && (timeinMin < 960)) {
+            yolo.innerHTML = "Stenger snart"
+        }
+        else if ((timeinMin > 540) && (timeinMin < 960)) {
+            yolo.innerHTML = "Åpent nå"
+        }
+        else {
+            yolo.innerHTML = "Stengt"
+        }
+    }
+
+    else {
+        yolo.innerHTML = "Stengt"
+    }
 }
 
-image1.addEventListener("onclick", link)
+window.addEventListener("load",checkOpen)
