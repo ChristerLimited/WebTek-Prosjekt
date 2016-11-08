11bee93cd8eb89b9dbf98e375f3ecc60056a3a9e
christholest <christ.holest@outlook.com>
2016-11-08 09:15:50 +0100

    resolved conflict and started fixing on product section

---
+++ Nettside_Main/pictureZoom.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+    const zoomedPicturesModules = document.getElementsByClassName("zoomed_pictures");
+    const zoomedPictures = document.getElementsByClassName("zoomed_pictures_content");
+    const startImages = document.getElementsByClassName("start_image");
+    const closeSpans = document.getElementsByClassName("close");
+
+    for (i = 0; i < zoomedPicturesModules.length; i++) {
+        const currentModule = zoomedPicturesModules[i];
+        const currentModalImage = zoomedPictures[i];
+        const currentModuleChildren = currentModule.childNodes;
+        const currentImage = startImages[i];
+        const currentCloseSpan = closeSpans[i];
+        currentCloseSpan.onclick = function() {
+            currentModule.style.display = "none";
+        };
+        currentImage.onclick = function() {
+            currentModule.style.display = "block";
+            currentModalImage.src = currentImage.src;
+        }
+    }
+};
